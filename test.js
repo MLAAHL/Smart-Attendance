38e4be30224d70409ce8618500f95af033281ba2
Praveen <itzzpchere@gmail.com>
2025-09-17 02:48:38 +0530

    core working good

--- test.js
+++
@@ -1,6 +0,0 @@
-const sendWhatsAppMessage = require('./utils/sendWhatsAppMessage');
-
-const number = "+917483729869"; // Add country code + number (India code 91)
-const message = "Hello! This is a test WhatsApp message from Twilio sandbox.";
-
-sendWhatsAppMessage(number, message);
